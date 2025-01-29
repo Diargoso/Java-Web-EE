65a7ac441fd03acff73c59bbdb7cb6bad4b43a92
STUDENT 1 <STUDENT 1@student1>
2025-01-29 12:03:56 +0300

    Доработана форма регистрации.

--- Potanin25-EE-war/src/java/controller/aouth.java
+++ Potanin25-EE-war/src/java/controller/aouth.java
@@ -20,8 +20,65 @@ public class aouth implements Serializable {
     /**
      * Creates a new instance of registrationBean
      */
-    public aouth() {
-        
-    }
+    String salutation, firstName, lastName, email;
+    int age;
+
+    public aouth() {}
     
+    public aouth(String salutation, String firstName, String lastName, String email, int age) {
+        this.salutation = salutation;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void emptyVariables() {
+        salutation = "";
+        firstName = "";
+        lastName = "";
+        age = 0;
+        email = "";
+    }
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
